bf76dbc5b020f23a08fe68ffdc16f9667afb3fa2
Marcel Costa <marcelcosta@gmail.com>
2023-11-08 21:29:05 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    if(this.items.length>1){
+      this.items.sort(function (a, b) {
+      return a - b;
+    });
+    }
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    let suma = 0;
+    for(let i = 0; i<this.items.length; i++){
+      suma += this.items[i]
+    }
+    return suma
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.sum()/this.items.length
+  }
 }
 
 module.exports = SortedList;
